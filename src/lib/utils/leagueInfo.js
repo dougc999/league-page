2779a0d53c773ade45bfcd15f9b4079c3a230cd5
dougc999 <82682974+dougc999@users.noreply.github.com>
2023-10-25 15:54:58 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome to Hell Nerds</p>
+  <p>Welcome to hell Nerds</p>
 `;
 
 /*   STEP 3   */
